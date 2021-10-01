27faa9a136f637d0b0d954a5d43b8b98292d2a57
Nelson Maldonado <play2nino@gmail.com>
2021-09-30 23:45:13 -0700

    header rendered

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -8,7 +8,11 @@ test("renders without errors", () => {
   render(<ContactForm />)
 })
 
-test("renders the contact form header", () => {})
+test("renders the contact form header", () => {
+  render(<ContactForm />)
+  const header = screen.getByText("Contact Form")
+  expect(header).toHaveTextContent(/Contact Form/i)
+})
 
 test("renders ONE error message if user enters less then 5 characters into firstname.", async () => {})
 
